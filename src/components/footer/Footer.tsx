aa8b67d5744475eb80b3e2b9176df0ffcb846d9c
Hassan <hassan.bmw2251@gmail.com>
2024-08-27 00:51:47 +0300

    footer styles

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,12 +1,18 @@
 import React, { type PropsWithChildren, type FunctionComponent } from "react";
 import styles from "./styles/SFooter.module.scss";
+import Link from "next/link";
 
 export interface IFooter {}
 
 export const Footer: FunctionComponent<IFooter> = ({}) => {
   return (
     <footer className={styles.footer}>
-      Footer component with sass modules
+      <p>&copy; 2024 By: Hassan Mohamed. All rights reserved.</p>
+
+      {/* <div className={styles.menus}>
+        <Link href="#">About</Link>
+        <Link href="#">Contact Us</Link>
+      </div> */}
     </footer>
   );
 };
